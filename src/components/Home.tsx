f8cb0e57f2e300b43071dda1271569ff807464f8
wenhaoming <435203093@qq.com>
2021-05-25 00:10:27 +0800

    style: 修改页面样式

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC<Omit<IRouteComponentProps, 'children'>> = props => {
     return (
         <ul className="list">
             {
-                ((props.route || {}).routes || []).filter(item=>item.path !== '/').sort((a,b)=>b.meta.updatedTime - a.meta.updatedTime).map((item, idx) => (item.title && <li key={idx} className="list-item">
+                ((props.route || {}).routes || []).filter(item=>!item.meta.writing).filter(item => item.path !== '/').sort((a,b)=>b.meta.updatedTime - a.meta.updatedTime).map((item, idx) => (item.title && <li key={idx} className="list-item">
                     <time className="item-update">{moment(item.meta.updatedTime).format('l')}</time>
                     <p onClick={()=>{props.history.push(item.path);window.scrollTo(0,0)}} className="item-title">{item.title}</p>
                 </li>))
